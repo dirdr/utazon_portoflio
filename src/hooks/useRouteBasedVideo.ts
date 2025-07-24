bd879b0f0f977db5546a98ae8847b8b73c8ca3db
dirdr <adrien.pelfresne@gmail.com>
2025-07-24 10:25:12 +0200

    fix(build) build

--- src/hooks/useRouteBasedVideo.ts
+++ src/hooks/useRouteBasedVideo.ts
@@ -2,13 +2,14 @@ import { useLocation } from "wouter";
 
 export const useRouteBasedVideo = () => {
   const [location] = useLocation();
-  
+
   const shouldPlayVideo = location === "/";
   const shouldShowLayout = location !== "/";
-  
+
   return {
     shouldPlayVideo,
     shouldShowLayout,
     currentPath: location,
   };
 };
+
